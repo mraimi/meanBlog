4a62c18643faf1f2ff14728cd53e9e46a5762e05
Michael Raimi <mar2260@columbia.edu>
2016-02-15 21:52:39 -0500

    Add comments to server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var connect = require('connect');
 var app = connect();
 
+// Handles request and response
 var helloWorld = function(req,res,next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end("Hello World");
 };
+
+// Register middleware with Connect application
 app.use(helloWorld);
 
 app.listen(3000);
