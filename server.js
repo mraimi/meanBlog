7550b07f9bd5952119f866d96f6fa61b72771f97
Michael Raimi <mar2260@columbia.edu>
2016-02-15 21:47:05 -0500

    Rewrite server with Connect middleware

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var http = require('http');
+var connect = require('connect');
+var app = connect();
 
-http.createServer(function(req,res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World');
-}).listen(3000);
+var helloWorld = function(req,res,next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello World");
+};
+app.use(helloWorld);
 
+app.listen(3000);
 console.log('Server running at http://localhost:3000/');
