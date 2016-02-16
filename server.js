0597d0b5626f3248751c8ca2cd6e4633523db041
Michael Raimi <mar2260@columbia.edu>
2016-02-15 22:00:41 -0500

    Add logger middleware

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var connect = require('connect');
 var app = connect();
 
+
+var logger = function(req, res, next) {
+    console.log(req.method, req.url);
+
+    next();
+};
+
 // Handles request and response
-var helloWorld = function(req,res,next) {
+var helloWorld = function(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end("Hello World");
 };
 
-// Register middleware with Connect application
+app.use(logger);
 app.use(helloWorld);
+// Register middleware with Connect application
 
 app.listen(3000);
+
 console.log('Server running at http://localhost:3000/');
